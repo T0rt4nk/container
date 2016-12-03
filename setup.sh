4f3bc890ad6f3f58966493cc80a2c1a8d575c26d
Maxime Vidori <maxime.vidori@gmail.com>
2016-12-03 14:01:08 +0100

    Correct setup of max user

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -xe
 
-declare apt_options="-o Acquire::Retries=10 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold --force-yes -y"
 export DEBIAN_FRONTEND="noninteractive"
+declare apt_options="-o Acquire::Retries=10 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold --force-yes -y"
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6616109451BBBF2
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A040830F7FAC5991
@@ -17,14 +17,14 @@ declare packages=(
 	"google-chrome-stable" "grub" "ssh"
 	# "steam" not working right now removing
 	# Python
-	"python-dev" "ipython" "python-pip" "python-distlib-whl"
+	"python-pip" "python-dev" "ipython" "python-distlib-whl"
 	# Cinnamon
 	"xserver-xorg" "x11-xserver-utils" "xfonts-base" "xinit"
     "lightdm-gtk-greeter" "cinnamon-core" "libgl1-mesa-dri" "dmz-cursor-theme"
     "nvidia-driver"
 )
 declare packages_unstable=(
-	"libmsgpackc2" "neovim" "virtualenv" "libtinfo5:i386"
+	"neovim"
 )
 
 apt-get ${apt_options[@]} install -t unstable "${packages_unstable[@]}"
@@ -33,9 +33,9 @@ debconf-set-selections <<< "keyboard-configuration  keyboard-configuration/xkb-k
 debconf-set-selections <<< "keyboard-configuration  keyboard-configuration/compose  select  No compose key"
 debconf-set-selections <<< "keyboard-configuration  keyboard-configuration/modelcode  string  pc105"
 
-debconf-set-selections <<< "steam   steam/license   note"
-debconf-set-selections <<< "steam   steam/purge     note"
-debconf-set-selections <<< "steam   steam/question  select  I AGREE"
+debconf-set-selections <<< "steam	steam/purge	note	"
+debconf-set-selections <<< "steam	steam/license	note	"
+debconf-set-selections <<< "steam	steam/question	select	I AGREE"
 
 debconf-set-selections <<< "locales locales/default_environment_locale select en_US.UTF-8"
 
@@ -46,7 +46,7 @@ declare packages_pip=("pdbpp" "path.py")
 pip install "${packages_pip[@]}"
 
 ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
-locale-gen
+
 dpkg-reconfigure --frontend=noninteractive tzdata
 dpkg-reconfigure --frontend=noninteractive locales
 
@@ -55,5 +55,6 @@ if ! getent passwd max; then
 	chown -R max:users /home/max
 	sudo -u max xdg-user-dirs-update
 	sudo -u max vim +PluginInstall +qall
+	cd /home/max/documents/development/dotfiles && sudo -u max make && cd -
 	trap "passwd max" EXIT
 fi
